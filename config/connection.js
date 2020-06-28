4ad89079ec3a163a11e2adaca068fe1fe61e9055
Medric Riley <medricr@gmail.com>
2020-06-27 19:44:53 -0700

    initial test schema written

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
 	host: 'localhost',
-	port: 3030,
+	port: 3306,
 	user: 'root',
-	password: 'password',
+	password: 'root',
 	database: 'adHooks'
 });
 
