b80df0c1f0706509099af6c0019b02d8771a020c
Medric Riley <medricr@gmail.com>
2020-06-28 12:10:12 -0700

    added initial react app folder, updated scripts in root

--- schema.sql
+++ schema.sql
@@ -1,11 +1,18 @@
 CREATE DATABASE adHooks;
 USE adHooks;
 
-CREATE TABLE `chirps` (
-  `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
-  `author` VARCHAR( 255) NOT NULL,
-  `body` VARCHAR( 255 ) NOT NULL,
-  `created_at` DATETIME NOT NULL,
+-- CREATE TABLE `chirps` (
+--   `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+--   `author` VARCHAR( 255) NOT NULL,
+--   `body` VARCHAR( 255 ) NOT NULL,
+--   `created_at` DATETIME NOT NULL,
 
-  PRIMARY KEY ( `id` ) 
+--   PRIMARY KEY ( `id` ) 
+-- );
+
+CREATE TABLE `pieces` (
+	`id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+	`info_source` VARCHAR( 255 ) NOT NULL,
+
+	PRIMARY KEY ( `id` )
 );
