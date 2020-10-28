6da6eb8bed6541b278aee8702253a4a4f45984df
Medric Riley <medricr@gmail.com>
2020-10-28 16:46:43 -0700

    added homepage template and api.js doc

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+		<Splashpage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue';
+
+import Splashpage from './components/Splashpage';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+		Splashpage
   }
 }
 </script>
---
+++ client/src/components/Splashpage.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<button>Click Me</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Splashpage"
+}
+</script>
+
+<style scoped>
+
+</style>
